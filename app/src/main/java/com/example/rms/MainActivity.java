73e438a8eff4dbcba3a2452c1ee775a3de7cc22f
Sricharan16 <cs16btech11044@iith.ac.in>
2020-04-03 06:14:36 +0530

    Basic Login & SignUp functionality added

---
+++ app/src/main/java/com/example/rms/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.rms;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnSignIn,btnSignUp;
+    TextView txtSlogan;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+        btnSignIn=(Button)findViewById(R.id.btnSignIn);
+                btnSignUp=(Button)findViewById(R.id.btnSignUp);
+                txtSlogan=(TextView)findViewById(R.id.txtSlogan);
+        Typeface face=Typeface.createFromAsset(getAssets(),"fonts/Oswald-Bold.ttf");
+        txtSlogan.setTypeface(face);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signUp = new Intent(MainActivity.this,SignUp.class);
+                startActivity(signUp);
+            }
+        });
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent signIn = new Intent(MainActivity.this,SignIn.class);
+                startActivity(signIn);
+            }
+        });
+    }
+}
